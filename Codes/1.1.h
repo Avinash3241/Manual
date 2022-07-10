86a55c9f8f6dfa1deea893e9eff4842d149b395f
Avinash3241 <102507619+Avinash3241@users.noreply.github.com>
2022-07-10 23:19:15 +0530

    Add files via upload

---
+++ Codes/1.1.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void uniform(char *str, int len);
+
+void uniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
---
+++ Codes/2.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "2.1.h"
+
+int main(){
+
+gaussian("gau.dat",1000000);
+
+return 0;
+}
+
